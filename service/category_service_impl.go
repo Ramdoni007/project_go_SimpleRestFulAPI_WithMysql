15a5e44378b9c3a3c076cf679a29c1de8cef54c6
Ramdoni007 <ramd42156@gmail.com>
2022-06-05 16:00:31 +0700

    Added category_implementation file in my project

--- service/category_service_impl.go
+++ service/category_service_impl.go
@@ -1,7 +1,46 @@
 package service
 
-import "Ramdoni007/Learn-Golang-RestFullAPI/repository"
+import (
+	"Ramdoni007/Learn-Golang-RestFullAPI/helper"
+	"Ramdoni007/Learn-Golang-RestFullAPI/model/web "
+	"Ramdoni007/Learn-Golang-RestFullAPI/repository"
+	"context"
+	"database/sql"
+)
 
 type CategoryServiceImpl struct {
-	repository.CategoryRepository
+	CategoryRepository repository.CategoryRepository
+	DB                 *sql.DB
 }
+
+func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryRespone {
+	tx,err := service.DB.Begin()
+	helper.PanicIfError(err)
+
+	defer func() {
+		err := recover()
+		if err != nil {
+			tx.Rollback()
+		}else {
+			tx.Commit()
+		}
+	}()
+
+}
+
+func (service *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryRespone {
+
+}
+
+func (service *CategoryServiceImpl) Delete(ctx context.Context, categoryId int) {
+	panic("implement me")
+}
+
+func (service *CategoryServiceImpl) FindById(ctx context.Context, categoryId int) web.CategoryRespone {
+	panic("implement me")
+}
+
+func (service *CategoryServiceImpl) FindAll(ctx context.Context) []web.CategoryRespone {
+	panic("implement me")
+}
+
